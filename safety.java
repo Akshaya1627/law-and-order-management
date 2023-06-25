4eb53d3b53ae755222154b46c808e78d2d8f92cf
Akshaya1627 <137667550+Akshaya1627@users.noreply.github.com>
2023-06-25 09:13:56 +0530

    Add files via upload

---
+++ safety.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package example;
+
+/**
+ *
+ * @author admin
+ */
+public class safety {
+    public static void main(String args[])
+    {
+        title tr=new title();
+        tr.setVisible(true);
+    }
+}
